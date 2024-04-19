c40c60a4d8fe8d8851981498035099b6de3d40a2
PRATYUSH-MISH <pratoss32@gmail.com>
2024-04-19 13:56:33 +0530

    Login_signup

---
+++ client/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import  './App.css';
+import Home from "./Home"
+import Login from "./Login"
+import Signup from './Signup';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </Router>
+    </div> 
+  )
+}
+
+export default App
